27655f3d7fa3de7231f0d6c4ae078e7881d7f1fb
Zpub <lil.rhymes9@gmail.com>
2023-12-06 22:48:53 +0300

    Initial commit

---
+++ src/main/java/com/example/taskmanagerspringtest/TaskManagerSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskmanagerspringtest/TaskManagerSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
